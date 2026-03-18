5962e0b95448a0ac95f251f0feb58e7bfffd569a
Jack Bennett <jdb@kosude.net>
2026-03-18 18:35:05 +0000

    Add font size argument with absolute and percentage units

--- attributor.py
+++ attributor.py
@@ -4,9 +4,11 @@
 #
 #   See the LICENCE file for more information.
 
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 
 import argparse
+import os
+import re
 
 # configure and parse CLI arguments
 parser = argparse.ArgumentParser()
@@ -27,9 +29,15 @@ parser.add_argument("-f",
                     choices=["dos"],
                     dest="font",
                     default="dos")
+parser.add_argument("-s",
+                    type=str,
+                    help="specify the font size (defaults to 30%% of the " \
+                         "image width)",
+                    dest="fontsize",
+                    default="30%")
 args = parser.parse_args()
 
-# validate conditionally required arguments
+# validate arguments
 if args.inplace and args.output is not None:
     parser.error("The flags -o and -i cannot be used simultaneously")
     exit(1)
@@ -49,12 +57,76 @@ except Exception as e:
     print(f"Error when loading image at {args.input}: {e}")
     exit(2)
 
-font = ImageFont.truetype(fontpath, 30)
+# TODO take text input
+text = "testtext"
+
+# get the input font size as formatted
+fontspec_size = 0
+fontspec_unit = ""
+try:
+    fontspec_size, fontspec_unit = \
+        re.findall(r"^([0-9.]+)([^0-9\s]*)$", args.fontsize)[0]
+except IndexError:
+    print(f"Error: invalid/malformed font size specifier \"{args.fontsize}\"")
+    exit(1)
+
+fontspec_size = float(fontspec_size)
+
+# font size format validation
+if fontspec_unit == "" or fontspec_unit is None:
+    print(f"Error: no unit given in font size \"{args.fontsize}\"")
+    exit(1)
+if fontspec_size < 0.005: # i.e. roughly zero (float)
+    print(f"Error: specified font size (\"{args.fontsize}\") cannot be zero")
+    exit(1)
+
+# figure out the most ideal actual font size
+fontsize = 0
+match fontspec_unit:
+    # points taken to be absolute value
+    case "pt":
+        fontsize = fontspec_size
+
+    # percentage fontsize taken to be a proportion of the image size
+    case "%":
+        _fontprop = fontspec_size * 0.01 # convert percentage to decimal
+
+        # get the font size based on proportion of the image to take up
+        # a method courtesy of Paul on SO:
+        # https://stackoverflow.com/a/4902713/12980669
+        _imgwid = img.size[0]
+        while True:
+            fontsize += 1
+
+            # attempt to load font at this size
+            try:
+                font = ImageFont.truetype(fontpath, fontsize)
+            except Exception as e:
+                print(f"Error when loading font from {fontpath}: {e}")
+                exit(4)
+
+            # get width of rendered text
+            _l, _, _r, _ = font.getbbox(text)
+            _wid = _r - _l
+
+            # repeat with incrementing fontsize until the text is big enough
+            if _wid >= _fontprop * _imgwid:
+                break
+
+    case _:
+        print(f"Invalid unit \"{fontspec_unit}\" specified in font size " \
+              f"argument \"{args.fontsize}\"")
+        exit(1)
+
+# load the font at the final size
+# yes, this is unnecessary when percentage units given, but it isn't too
+# expensive soo oh well!
+font = ImageFont.truetype(fontpath, fontsize)
 
 # draw text to image
 draw = ImageDraw.Draw(img)
 
-draw.text((0, 0), "jerma985", font=font)
+draw.text((0, 0), text, font=font)
 
 # if just previewing the output, then show the image and exit cleanly
 if preview:
